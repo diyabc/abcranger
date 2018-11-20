05f8856f088be8c6b1ca176045ac3ac84664422a
François-David Collin <Francois-David.Collin@umontpellier.fr>
2018-11-20 23:14:21 +0100

    PLS Ok

--- pls-eigen-test.cpp
+++ pls-eigen-test.cpp
@@ -1,8 +1,70 @@
 #define BOOST_TEST_MODULE PLSEigenTest
 #include <boost/test/unit_test.hpp>
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
 #include "pls-eigen.hpp"
 
+void split(const string& s, char c, vector<string>& v) {
+    string::size_type i = 0;
+    string::size_type j = s.find(c);
+
+    while (j != string::npos) {
+        v.push_back(s.substr(i, j-i));
+        i = ++j;
+        j = s.find(c, j);
+    }
+    if (j == string::npos) v.push_back(s.substr(i, s.length( )));
+}
+
+inline double string2double(const std::string& s){ std::istringstream i(s); double x = 0; i >> x; return x; }
+
+MatrixXd read_matrix_file(string filename, char sep) {
+    cerr << "Loading " << filename << endl;
+    ifstream myfile(filename.c_str());
+    stringstream ss;
+
+    vector<vector<double> > M;
+    if (myfile.is_open()) {
+        string line;
+
+        while ( getline(myfile,line) ) {
+            //split string based on "," and store results into vector
+            vector<string> fields;
+            split(line, sep, fields);
+
+            vector<double>row(fields.size());
+            for( int i=0; i < fields.size(); i++ ) {
+                row[i] = string2double(fields[i]);
+            }
+            M.push_back(row);
+        }
+    }
+
+    MatrixXd X( (int) M.size(), (int) M[0].size() );
+    for(int i=0; i < M.size(); i++ ) {
+        for(int j=0; j < M[i].size(); j++ ) {  
+            X(i,j)=M[i][j]; 
+        }
+    }
+    return X;
+}
+
 BOOST_AUTO_TEST_CASE(PLSEigenTestSimple, *boost::unit_test::tolerance(1e-7)) {
-    BOOST_CHECK(1 == 1);
+    MatrixXd X = read_matrix_file("gasolineX.csv",',');
+    VectorXd Y = read_matrix_file("gasolineY.csv",',');
+
+    MatrixXd Pls;
+    VectorXd res = pls(X,Y,10,0.99,Pls);
+    VectorXd exp(6);
+    exp << 0.3054273,
+           0.7979361,
+           0.9773195,
+           0.9826665,
+           0.9867306,
+           0.9890077;
+    BOOST_TEST((res - exp).lpNorm<Infinity>() == 0.0);
 }
--- pls-eigen.hpp
+++ pls-eigen.hpp
@@ -18,3 +18,39 @@
 
 using namespace Eigen;
 using namespace std;
+
+VectorXd pls(const MatrixXd &x,
+         const VectorXd &y,
+         size_t ncomp,
+         double threshold,
+         MatrixXd &Pls)
+{
+    auto n = x.rows();
+    auto p = x.cols();
+    auto r = ncomp;
+    RowVectorXd mean = x.colwise().mean();
+    RowVectorXd std = ((x.rowwise() - mean).array().square().colwise().sum() / (x.rows() - 1)).sqrt();;
+    MatrixXd X = (x.rowwise() - mean).array().rowwise() / std.array();
+    MatrixXd Y = MatrixXd::Zero(n, ncomp + 1);
+    MatrixXd Z(n,ncomp);
+    // Y.col(0) = y.rowwise() - y.colwise().mean();
+    double ymean = y.mean();
+    Y.col(0).array() = ymean;
+    double SSTO = (y.array() - ymean).array().square().sum();
+    for (auto m = 0; m < ncomp; m++)
+    {
+        VectorXd Zm = X * X.transpose() * y;
+        double Znorm = Zm.array().square().sum();
+        Z.col(m) = Zm;
+        double Thetam = Zm.dot(y) / Znorm;
+        Y.col(m + 1) = Y.col(m) + Thetam * Zm;
+        if ((Y.col(m+1).array() - ymean).array().square().sum() / SSTO >= threshold) {
+            r = m - 1;
+            break;
+        }
+        X -= Zm.rowwise().replicate(p) * ((Zm/Znorm).transpose() * X).asDiagonal();
+    }
+    Pls = Z.block(0,0,n,r);
+    VectorXd res = (Y.block(0,1,n,r+1).array() - ymean).array().square().colwise().sum() / SSTO;
+    return res;
+}

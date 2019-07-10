5a51cda20c06245b8a4b8a5ffe900deda7599c5a
François-David Collin <Francois-David.Collin@umontpellier.fr>
2019-07-10 16:29:21 +0200

    Readme update

--- abcranger.cpp
+++ abcranger.cpp
@@ -29,7 +29,7 @@ int main(int argc, char* argv[]) {
             ("c,noisecolumns","Number of noise columns",cxxopts::value<size_t>()->default_value("5"))
             ("nolinear","Disable LDA for model choice or PLS for parameter estimation")
             ("chosenscen","Chosen scenario (mandatory for parameter estimation)", cxxopts::value<size_t>())
-            ("ntest","number of testing samples (mandatory for parameter estimation)",cxxopts::value<size_t>())
+            ("ntest","number of oob testing samples (mandatory for parameter estimation)",cxxopts::value<size_t>())
             ("parameter","name of the parameter of interest (mandatory for parameter estimation)",cxxopts::value<std::string>())
             ("help", "Print help")
             ;

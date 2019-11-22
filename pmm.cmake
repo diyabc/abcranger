b589299264b75fa39e6c3605626f5857a12dfd5b
François-David Collin <Francois-David.Collin@umontpellier.fr>
2019-11-22 10:08:56 +0100

    vcpkg PR ok, reverted to origin for pmm

--- pmm.cmake
+++ pmm.cmake
@@ -37,7 +37,7 @@ _pmm_set_if_undef(PMM_VERSION ${PMM_VERSION_INIT})
 # The base URL we download PMM from:
 _pmm_set_if_undef(PMM_URL_BASE "http://anotherfoxguy.com/pmm/")
 # The real URL we download from (Based on the version)
-_pmm_set_if_undef(PMM_URL "https://raw.githubusercontent.com/fradav/pmm/master/pmm/")
+_pmm_set_if_undef(PMM_URL "${PMM_URL_BASE}/${PMM_VERSION}")
 # The directory where we store our downloaded files
 _pmm_set_if_undef(PMM_DIR_BASE "${CMAKE_BINARY_DIR}/_pmm")
 _pmm_set_if_undef(PMM_DIR "${PMM_DIR_BASE}/${PMM_VERSION}")

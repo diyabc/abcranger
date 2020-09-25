65b73ef50c384a551d36ef8eea2f46e46d9371f1
François-David Collin <Francois-David.Collin@umontpellier.fr>
2020-09-25 14:55:37 +0200

    moved all sudo

--- presetup.sh
+++ presetup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 pip install cmaketools pybind11 cmake ninja
-sudo apt-get -y install wget
+apt-get -y install wget
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
-sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
-sudo sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'
-sudo apt-get update
-sudo apt-get -y install intel-mkl-64bit-2020.0-088
+apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'
+apt-get update
+apt-get -y install intel-mkl-64bit-2020.0-088
 wget https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
 tar xvf clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz

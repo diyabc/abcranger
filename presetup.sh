103fd08eef27e375d85f7b3560a481c40e3127e8
François-David Collin <Francois-David.Collin@umontpellier.fr>
2020-09-28 14:44:30 +0200

    specifiying python3 in centos image

--- presetup.sh
+++ presetup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-pip install cmaketools pybind11 cmake ninja
 yum-config-manager --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
 rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
 yum install -y intel-mkl-64bit-2020.0-088
+yum install -y python3-devel
+pip3 install cmaketools pybind11 cmake ninja

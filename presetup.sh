d10c28e039269c21e8c6940e1193f40d511d8089
François-David Collin <Francois-David.Collin@umontpellier.fr>
2020-09-28 14:11:21 +0200

    remove clang and force manylinux2014

--- presetup.sh
+++ presetup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 pip install cmaketools pybind11 cmake ninja
-yum install -y wget
 yum-config-manager --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
 rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
 yum install -y intel-mkl-64bit-2020.0-088
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-tar xvf clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz

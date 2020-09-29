5f784aac605deb0ea51cc84ed05cea79eeac2c30
François-David Collin <Francois-David.Collin@umontpellier.fr>
2020-09-29 12:30:36 +0200

    use self made docker image

--- presetup.sh
+++ presetup.sh
@@ -2,4 +2,3 @@
 yum-config-manager --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
 rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
 yum install -y intel-mkl-64bit-2020.0-088
-pip install cmaketools cmake ninja

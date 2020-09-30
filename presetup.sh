a2474d148a52258893157c5724943ff7383243f4
François-David Collin <Francois-David.Collin@umontpellier.fr>
2020-09-30 06:45:08 +0200

    typo in pypy skip

--- presetup.sh
+++ presetup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-yum-config-manager --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
-rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
-yum install -y intel-mkl-64bit-2020.0-088
+if [ -x "$(command -v yum-config-manager)" ]; then
+    yum-config-manager --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
+    rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+    yum install -y intel-mkl-64bit-2020.0-088
+fi

e0278e947866407c7c47c01d22f05858fa9520ea
fradav <fradav@gmail.com>
2020-10-26 22:41:58 +0100

    setup.py ignores build args

--- setup.py
+++ setup.py
@@ -6,15 +6,12 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
-build_dir="build"
-
 configure_opts = ["""--no-warn-unused-cli -DMAKE_STATIC_EXE:BOOL=TRUE -DUSE_MKL:BOOL=TRUE -DLAPACK_ROOT:STRING=/opt/intel/mkl/lib/intel64 "-DLAPACK_LIBRARIES:STRING=-Wl,--start-group /opt/intel/mkl/lib/intel64/libmkl_intel_lp64.a /opt/intel/mkl/lib/intel64/libmkl_tbb_thread.a /opt/intel/mkl/lib/intel64/libmkl_core.a -Wl,--end-group\;pthread\;m\;dl" "-DBLAS_LIBRARIES:STRING=-Wl,--start-group /opt/intel/mkl/lib/intel64/libmkl_intel_lp64.a /opt/intel/mkl/lib/intel64/libmkl_tbb_thread.a /opt/intel/mkl/lib/intel64/libmkl_core.a -Wl,--end-group\;pthread\;m\;dl" -DCMAKE_BUILD_TYPE:STRING=Release -G Ninja ../"""]
 if((len(sys.argv) > 1) and (sys.argv[1] == "sdist")):
         configure_opts = ["""--no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -G Ninja ../"""]
 elif(getenv("CMAKEARGSFILE") and path.isfile(getenv("CMAKEARGSFILE"))):
     with open(path.join(this_directory, getenv("CMAKEARGSFILE")), encoding='utf-8') as f:
         configure_opts = f.read()
-        build_dir = getenv("BUILDDIR")
 
 setup(
     name="pyabcranger",
@@ -32,5 +29,5 @@ setup(
     has_package_data=False,
     packages=["pyabcranger"],
     configure_opts=configure_opts,
-    build_opts=["--build",build_dir,"--target","pyabcranger","--config","Release"]
+    build_opts=["--target","pyabcranger","--config","Release"]
 )

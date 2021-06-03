1557f6b926521795b1d99a1a7b9756710ee92426
François-David Collin <Francois-David.Collin@umontpellier.fr>
2021-06-03 10:45:10 +0200

    [pypi] update

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ else:
 
 setup(
     name="pyabcranger",
-    version="0.0.50",
+    version="0.0.51",
     author="François-David Collin",
     author_email="fradav@gmail.com",
     description="ABC random forests for model choice and parameter estimation, python wrapper",

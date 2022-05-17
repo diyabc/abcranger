1f232cf8cf8df6b3dd683878707d2483c7506250
François-David Collin <Francois-David.Collin@umontpellier.fr>
2022-05-17 15:33:37 +0200

    [skip-ci] manylinux upgrade to deb

--- presetup.sh
+++ presetup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-if [ -x "$(command -v yum-config-manager)" ]; then
-    yum-config-manager --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
-    rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
-    yum install -y intel-mkl-64bit-2020.0-088
-    yum install -y curl zip unzip tar
-fi
+# if [ -x "$(command -v yum-config-manager)" ]; then
+#     yum-config-manager --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
+#     rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+#     yum install -y intel-mkl-64bit-2020.0-088
+ 
+# fi
+cd /tmp
+wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+rm GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+wget https://apt.repos.intel.com/setup/intelproducts.list -O /etc/apt/sources.list.d/intelproducts.list
+apt-get update
+apt-get install -y intel-mkl-64bit-2020.0-088 openssl

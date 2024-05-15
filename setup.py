837f865d6bd1a007c1fbd95473f49e82a0e19133
François-David Collin <Francois-David.Collin@umontpellier.fr>
2024-05-15 10:36:46 +0200

    [pypi] CI maintenance

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ else:
 
 setup(
     name="pyabcranger",
-    version="0.0.71",
+    version="0.0.72",
     author="François-David Collin",
     author_email="fradav@gmail.com",
     description="ABC random forests for model choice and parameter estimation, python wrapper",
